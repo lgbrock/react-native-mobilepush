8b9c1ba32e4c15b12a75a089c70ab75ae054d3d4
Logan Brock <logan.g.brock@gmail.com>
2022-10-07 13:49:50 -0400

    Added all components - testing form signup to MC

--- App.js
+++ App.js
@@ -1,80 +1,14 @@
-// SignUp.js
-import React, {useState} from 'react';
-import {View, Button, TextInput, StyleSheet} from 'react-native';
+import React from 'react';
+import {View, StyleSheet} from 'react-native';
 // import Main from './components/Main';
-import Registered from './components/Registered';
-import MCReactModule from './sfmc.d.ts';
+// import Registered from './components/Registered';
+import SignUpForm from './components/SignUpForm';
 
-export default class SignUp extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      first_name: 'Logan',
-      last_name: 'Brock',
-      email: 'lbrock@eigenx.com',
-      company: 'Eigen X',
-    };
-  }
-
-  register() {
-    const {first_name, last_name, email, company} = this.state;
-    const data = {
-      first_name: first_name,
-      last_name: last_name,
-      email: email,
-      company: company,
-    };
-    // set data from state
-    MCReactModule.setContactKeyValues(data);
-    // register contact
-    MCReactModule.registerContact();
-    console.log(data);
-  }
-
-  onChangeText = (key, val) => {
-    this.setState({[key]: val});
-  };
-
+export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Registered />
-        {/* <TextInput
-          style={styles.input}
-          placeholder="First Name"
-          autoCapitalize="none"
-          placeholderTextColor="white"
-          onChangeText={val => this.onChangeText('first_name', val)}
-        />
-        <TextInput
-          style={styles.input}
-          placeholder="Last Name"
-          autoCapitalize="none"
-          placeholderTextColor="white"
-          onChangeText={val => this.onChangeText('last_name', val)}
-        />
-
-        <TextInput
-          style={styles.input}
-          placeholder="Email"
-          autoCapitalize="none"
-          placeholderTextColor="white"
-          onChangeText={val => this.onChangeText('email', val)}
-        />
-        <TextInput
-          style={styles.input}
-          placeholder="Company"
-          autoCapitalize="none"
-          placeholderTextColor="white"
-          onChangeText={val => this.onChangeText('company', val)}
-        />
-        <Button title="Sign Up" onPress={this.handleRegisterButton()} />
-
-        <Button
-          style={styles.input}
-          title="Register"
-          onPress={() => this.register()}
-        /> */}
+        <SignUpForm />
       </View>
     );
   }
--- components/SignUpForm.jsx
+++ components/SignUpForm.jsx
@@ -1,7 +1,100 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {
+  Text,
+  TextInput,
+  View,
+  FlatList,
+  StyleSheet,
+  Button,
+} from 'react-native';
+
+import MCReactModule from './../sfmc.d.ts';
 
 const SignUpForm = () => {
-  return <div>SignUpForm</div>;
+  const [firstname, setFirstName] = useState('');
+  const [lastname, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [company, setCompany] = useState('');
+
+  handleSignUpButton = () => {
+    console.log(`'${firstname}' '${lastname}' '${email}' '${company}'`);
+    MCReactModule.registerContact(
+      firstname,
+      lastname,
+      email,
+      company,
+      (success, error) => {
+        if (success) {
+          console.log(
+            `Contact '${firstname}' '${lastname}' '${email}' '${company}' registered successfully`,
+          );
+        } else {
+          console.log('Error', error);
+        }
+      },
+    );
+  };
+
+  return (
+    <>
+      <View style={styles.container}>
+        <TextInput
+          style={styles.input}
+          placeholder="First Name"
+          autoCapitalize="none"
+          placeholderTextColor="white"
+          onChangeText={firstname => setFirstName(firstname)}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Last Name"
+          autoCapitalize="none"
+          placeholderTextColor="white"
+          onChangeText={lastname => setLastName(lastname)}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Email"
+          autoCapitalize="none"
+          placeholderTextColor="white"
+          onChangeText={email => setEmail(email)}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Company"
+          autoCapitalize="none"
+          placeholderTextColor="white"
+          onChangeText={company => setCompany(company)}
+        />
+        <Button
+          title="Confirm Email"
+          onPress={() =>
+            console.log(`'${firstname}' '${lastname}' '${email}' '${company}'`)
+          }
+          handleSignUpButton={handleSignUpButton}
+        />
+      </View>
+    </>
+  );
 };
+
+const styles = StyleSheet.create({
+  input: {
+    width: 350,
+    height: 55,
+    backgroundColor: '#42A5F5',
+    margin: 10,
+    padding: 8,
+    color: 'white',
+    borderRadius: 14,
+    fontSize: 18,
+    fontWeight: '500',
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
 
 export default SignUpForm;
